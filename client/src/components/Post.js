37bb5eccb1840922e503591e1c148be4b6a73b6e
Bhone Pyae Moie <bhonepyae.it@gmail.com>
2025-05-23 15:45:47 +0700

    Function added

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { IoMdMore } from "react-icons/io";
+import { AiOutlineLike } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
+import { FaRegShareSquare } from "react-icons/fa";
 
 function Post() {
   const [apiData, setApiData] = useState(null);
@@ -20,7 +23,7 @@ function Post() {
       {apiData &&
         apiData.map((item) => (
           <div
-            className="relative size-5/12 p-4 border rounded-lg shadow-md bg-white"
+            className="sm:size-5/12 relative size-11/12 px-4 pt-4 border rounded-lg shadow-md"
             key={item._id}
           >
             <div className="absolute top-4 right-4 text-2xl cursor-pointer">
@@ -38,6 +41,37 @@ function Post() {
               </div>
             </div>
             <p className="text-gray-800">{item.description}</p>
+
+            <div className="w-full mt-4 bg-blue-500">
+              {item.video &&
+              (item.video.endsWith("png") || item.video.endsWith("jpg")) ? (
+                <img src={item.video} className="w-full" />
+              ) : (
+                item.video && (
+                  <iframe
+                    src={item.video}
+                    className="w-full aspect-video mt-4 rounded-lg bg-cover"
+                    title="Video"
+                    allow="autoplay"
+                    allowFullScreen
+                  ></iframe>
+                )
+              )}
+            </div>
+            <div className="flex justify-evenly py-3">
+              <button className="w-full text-gray-500 hover:text-blue-500 py-2 hover:bg-gray-200 rounded-md flex justify-center items-center gap-2">
+                <AiOutlineLike />
+                Like
+              </button>
+              <button className="w-full text-gray-500 hover:text-blue-500 py-2 hover:bg-gray-200 rounded-md flex justify-center items-center gap-2">
+                <FaRegComment />
+                Comment
+              </button>
+              <button className="w-full text-gray-500 hover:text-blue-500 py-2 hover:bg-gray-200 rounded-md flex justify-center items-center gap-2">
+                <FaRegShareSquare />
+                Share
+              </button>
+            </div>
           </div>
         ))}
     </div>
--- server/main.js
+++ server/main.js
@@ -6,11 +6,12 @@ const PORT = 5000;
 const data = [
   {
     _id: 1,
-    name: "Peter",
-    profile: "Images/Image1.jpg",
+    name: "Ariana Grande",
+    profile: "Images/Image3.jpg",
     topic: "React",
-    description: "React is a JavaScript library for building user interfaces.",
+    description: "My new baby is out now. link in bio 💿✨",
     timestamp: "3d",
+    video: "https://www.youtube.com/embed/x1XIJM6spaE",
   },
   {
     _id: 2,
@@ -20,15 +21,39 @@ const data = [
     description:
       "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
     timestamp: "18h",
+    video:
+      "https://drive.google.com/file/d/1972Y7PuHqBRzX8_wxYqbA6YcgKG-2DV6/preview",
   },
   {
     _id: 3,
-    name: "Mohammed",
-    profile: "Images/Image1.jpg",
+    name: "React Coder",
+    profile: "Images/Image4.jpg",
     topic: "Express",
     description:
-      "Express is a minimal and flexible Node.js web application framework.",
-    timestamp: "1w",
+      "Minimal. Responsive. Personal. Check out the portfolio I coded from scratch using React.",
+    timestamp: "1h",
+    video: "https://i.ytimg.com/vi/g0HKvRj84Ak/hq720.jpg",
+  },
+  {
+    _id: 4,
+    name: "Selena",
+    profile: "Images/Image2.jpg",
+    topic: "MongoDB",
+    description: "In the ocean’s presence, she forgot to be anything but real.",
+    timestamp: "2w",
+    video:
+      "https://images.pexels.com/photos/7000565/pexels-photo-7000565.jpeg?cs=srgb&dl=pexels-marian-sol-miranda-32246321-7000565.jpg&fm=jpg",
+  },
+  {
+    _id: 5,
+    name: "Jane",
+    profile: "Images/Image1.jpg",
+    topic: "JavaScript",
+    description:
+      "JavaScript is a programming language that conforms to the ECMAScript specification.",
+    timestamp: "3w",
+    video:
+      "https://png.pngtree.com/background/20250106/original/pngtree-nice-picsart-cb-backgrounds-hd-phone-wallpaper-picture-image_15522905.jpg",
   },
 ];
 
